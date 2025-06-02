9a0632c735531d5af6b844f50b1c9fb707c8959b
CloneWith <clonewith@qq.com>
2025-06-03 00:48:40 +0800

    Import all element plus icons beforehand

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import ElementPlus from "element-plus";
 import { useDark } from "@vueuse/core";
 import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 import App from "./App.vue";
 import router from "./router";
@@ -17,5 +18,9 @@ useDark();
 app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 app.mount("#app");
